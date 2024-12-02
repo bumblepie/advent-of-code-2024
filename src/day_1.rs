57a680b0297d641e6b97a5c367f1e996e13cf854
Luke Wolyncewicz <luke.wolyncewicz@pushpay.com>
2024-12-03 11:58:01 +1300

    Initial setup, complete day 1 part 1

---
+++ src/day_1.rs
@@ -0,0 +1,63 @@
+﻿use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use itertools::Itertools;
+use regex::Regex;
+
+pub fn part_1(input_file: &str) {
+    let (list, other_list) = parse_file(input_file);
+    let result = sum_diffs(&list, &other_list);
+    println!("Sum of diffs: {}", result);
+}
+
+fn parse_file(input_file: &str) -> (Vec<u32>,Vec<u32>) {
+    let regex = Regex::new(r"(?<left>\d+)\s+(?<right>\d+)").unwrap();
+    let file = File::open(input_file).expect("file not found");
+    let lines: Result<Vec<_>, _> = io::BufReader::new(file).lines().collect();
+    let lines = lines.expect("Error reading lines");
+
+    let pairs = lines.into_iter().map(|line| {
+        if let Some(captures) = regex.captures(&line) {
+            return (
+                captures["left"].parse::<u32>().unwrap(),
+                captures["right"].parse::<u32>().unwrap()
+            );
+        }
+        panic!("Could not parse line {}", &line);
+    });
+
+    pairs.unzip()
+}
+
+fn sum_diffs(list: &Vec<u32>, other_list: &Vec<u32>) -> u32 {
+    let list_sorted = list.iter()
+        .sorted();
+    let other_sorted = other_list.iter().sorted();
+    list_sorted
+        .zip(other_sorted)
+        .map(|(left,right)| left.abs_diff(*right))
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use super::*;
+
+    #[rstest]
+    #[case("inputs/day-1-example.txt", (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3]))]
+    fn test_parsing(#[case] input_file: &str, #[case] expected_result: (Vec<u32>, Vec<u32>)) {
+        let (list, other_list) = parse_file(input_file);
+        for i in 0..expected_result.0.len() {
+            assert_eq!(list[i], expected_result.0[i]);
+            assert_eq!(other_list[i], expected_result.1[i]);
+        }
+    }
+
+    #[rstest]
+    #[case(vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3], 11)]
+    fn test_sum_diffs(#[case] list: Vec<u32>, #[case] other_list: Vec<u32>, #[case] expected_result: u32) {
+        let result = sum_diffs(&list, &other_list);
+        assert_eq!(result, expected_result);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod day_1;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    day: u8,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    part: u8,
+}
+fn main() {
+    let args = Args::parse();
+    match args.day {
+        1 => match args.part {
+            1 => day_1::part_1("inputs/day-1-input.txt"),
+            _ => panic!("Unknown part: {}", args.part),
+        }
+        _ => panic!("Unknown day: {}", args.day),
+    }
+}
